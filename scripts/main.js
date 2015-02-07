2a32c490c78538b2e3150481454443b28342d906
= <lasazawa@gmail.com>
2015-02-06 16:34:35 -0800

    more demo

--- scripts/main.js
+++ scripts/main.js
@@ -7,38 +7,42 @@ $(document).ready(function() {
   // $('.camera').css({'height':photoWidth + 'px'});
 
   $('.camera').hover(function() {
-    $(this).addClass('flash')
-    .delay(100)
-    .queue(function() {
-      var cameraBtn = $('.camera-btn-center').addClass('pressed');
-      setTimeout(function () {
-        cameraBtn.removeClass('pressed');
-      }, 200);
-      $(this).addClass('photo-captured');
-      $('.focus').toggleClass('focus');
-      hideCameraBtn();
-
-      });
+    var cameraBtn = $('.camera-btn-center').addClass('pressed');
+    setTimeout(function () {
+      cameraBtn.removeClass('pressed');
+    }, 200);
+    photoCapture();
 
     });
+});
 
-
-
+function photoCapture() {
+  $('.camera').addClass('flash')
+  .delay(500)
+  .queue(function(){
+    $('.camera').addClass('photo-captured');
+    $('.focus').toggleClass('focus');
+    hideCameraBtn();
   });
+}
 
 function hideCameraBtn(usePhoto) {
   $('.camera-btn, .camera-btn-center')
   .delay(1000)
-  .fadeOut(200);
+  .fadeOut(100);
   setTimeout(function(){
     $('.use-photo').append('<a class="retake-link left">Retake</a>');
     $('.use-photo').append('<a class="use-photo-link right">Use Photo</a>');
-  }, 2000);
-
-  // usePhoto();
+  }, 1200);
+  showStudents();
 }
 
-// function usePhoto() {
-//   $('.use-photo').delay(1000).append('<a class="use-photo-link">Use Photo</a>');
-//   console.log("usePhoto");
-// }
+function showStudents() {
+  setTimeout(function(){
+    $('.retake-link').fadeOut(1);
+    $('.use-photo-link').fadeOut(1);
+    $('.faces').addClass('show');
+  }, 2000);
+}
+
+
