45607b995ff36550bdb90dd37a44d683cded8571
= <lasazawa@gmail.com>
2015-02-07 13:44:58 -0800

    animations

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,11 @@ $(document).ready(function() {
 
   // var photoWidth = $('.camera').width();
   // $('.camera').css({'height':photoWidth + 'px'});
+  setTimeout(function(){
+    $('.header').addClass('slideUp show');
+  }, 1500);
 
-  $('.camera').hover(function() {
+  $('.phone').hover(function() {
     var cameraBtn = $('.camera-btn-center').addClass('pressed');
     setTimeout(function () {
       cameraBtn.removeClass('pressed');
@@ -31,18 +34,55 @@ function hideCameraBtn(usePhoto) {
   .delay(1000)
   .fadeOut(100);
   setTimeout(function(){
-    $('.use-photo').append('<a class="retake-link left">Retake</a>');
-    $('.use-photo').append('<a class="use-photo-link right">Use Photo</a>');
-  }, 1200);
+    $('.use-photo').append('<a class="retake-link left slideUp">Retake</a>');
+    $('.use-photo').append('<a class="use-photo-link right slideUp">Use Photo</a>');
+  }, 1000);
+  underLine();
+}
+
+function underLine() {
+  setTimeout(function(){
+    $('.use-photo-link').addClass('underline');
+  }, 2000);
   showStudents();
 }
 
 function showStudents() {
   setTimeout(function(){
+    $('.use-photo-link').addClass('underline');
+    $('.faces').addClass('show slideUp');
     $('.retake-link').fadeOut(1);
     $('.use-photo-link').fadeOut(1);
-    $('.faces').addClass('show');
-  }, 2000);
+  }, 2400);
+  selectStudent1();
 }
+
+function selectStudent1() {
+  setTimeout(function() {
+    $('.face1').addClass('selected');
+    $('.camera-btn-container').append('<a class="right send">Send</a>');
+  }, 3600);
+  selectStudent2();
+}
+function selectStudent2() {
+  setTimeout(function() {
+    $('.face2').addClass('selected');
+  }, 4000);
+  selectStudent3();
+}
+function selectStudent3() {
+  setTimeout(function() {
+    $('.face3').addClass('selected');
+  }, 4400);
+  sendPhoto();
+}
+
+function sendPhoto() {
+  setTimeout(function() {
+    $('.send').addClass('underline');
+  }, 5000);
+}
+
+
 
 
